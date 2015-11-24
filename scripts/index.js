378b3934967493b537e0c3ef4c9d1c0c1426d0ce
Ma Yanlong <mayanlong7@icloud.com>
2015-11-24 16:42:47 +0800

    利用js  date对象的特性 删除一些判断代码

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,8 @@ window.onload = function(){
   var $ = function(id){
     return document.getElementById(id);
   };
+
+  //判断某个日期是否为今天
   var isToday = function(){
     var d  = new Date();
     if(d.getFullYear() == date.getFullYear() &&
@@ -16,6 +18,7 @@ window.onload = function(){
     }
     return false;
   };
+
   var addClass = function(el,c){
     var t = el.className.split(' ');
     var dict = {};
@@ -32,58 +35,33 @@ window.onload = function(){
     for(var i = 0; i<t.length; i++){
       dict[t[i]] = true;
     }
-    delete dict[c];
-    var n = '';
-    for (var name in dict){
-      n += name + ' ';
-    }
+    delete dict[c]; var n = '';
+    for (var name in dict){n += name + ' ';}
     el.className = n;
   };
+
+  //是否为闰年
   var isLeapYear = function(Year) {
     if (  Year % 4 == 0 && Year % 100 != 0 || Year % 400==0 ){
       return true;
     } else { return false; }
   };
+
   var setmeiyuetianshu = function(year){
     if( isLeapYear(year) ){
       meiyuetianshu[1] = 29;
     }
   };
+  //页面刷新再画日历之前应该调用一下
+  setmeiyuetianshu();
+
+  //前一天
   var previousDay = function(){
-    var currentYear =  date.getFullYear(),
-        currentMonth = date.getMonth(),
-        currentDate  = date.getDate(),
-        targetMonth,targetYear, targetDate = currentDate - 1;
-    setmeiyuetianshu(currentYear);
-    if( targetDate == 0 ){
-      targetMonth = currentMonth - 1; targetYear = currentYear;
-      if(targetMonth == -1){
-        targetYear = currentYear - 1; targetMonth = 11;
-        setmeiyuetianshu(targetYear);
-      }
-      targetDate  = meiyuetianshu[targetMonth];
-    }else{
-      targetMonth =  currentMonth; targetYear  =  currentYear;
-    }
-    date = new Date(targetYear,targetMonth,targetDate);
+    date.setDate(date.getDate()-1);
   };
+  //后一天
   var nextDay = function(){
-    var currentYear =  date.getFullYear(),
-        currentMonth = date.getMonth(),
-        currentDate  = date.getDate(),
-        targetMonth,targetYear, targetDate = currentDate + 1;
-    setmeiyuetianshu(currentYear);
-    if( targetDate > meiyuetianshu[currentMonth] ){
-      targetMonth = currentMonth + 1; targetYear  = currentYear;
-      if(targetMonth == 12 ){
-        targetYear = currentYear + 1; targetMonth = 0;
-        setmeiyuetianshu(targetYear);
-      }
-      targetDate  = 1;
-    }else{
-      targetMonth =  currentMonth; targetYear  =  currentYear;
-    }
-    date = new Date(targetYear,targetMonth,targetDate);
+    date.setDate(date.getDate()+1);
   };
   var ondatechange = (function(){
     var currentcell;
@@ -95,13 +73,8 @@ window.onload = function(){
     return function(){
       if( currentcell ){
         removeClass( currentcell,'cell-hover' );
-        removeClass( currentcell,'today' );
       }
-      // if(isToday()){
-      //   addClass( $('day_'+date.getDate()),'today');
-      // }else{
       addClass( $('day_' + date.getDate() ) ,'cell-hover');
-      // }
       currentcell = $('day_'+date.getDate());
 
       daydate.innerHTML   = date.getDate();
@@ -109,12 +82,12 @@ window.onload = function(){
       fulldate.innerHTML  = formate();
     };
   })();
+
   var drawcalender = function(){
     setmeiyuetianshu();
     var d = date.getDate();
     var month  = date.getMonth(); date.setDate(1);
     var xingqi = date.getDay();   date.setDate(d);
-
     var L =  ( xingqi - 1 ) == -1 ? 6 : ( xingqi - 1 ); var i;
     //画前一个月的天
     for (  i = 0;  i < L;  i++){
@@ -141,11 +114,13 @@ window.onload = function(){
     for ( ;  i < 42;  i++){
       cells[i].removeAttribute('prev'); cells[i].removeAttribute('next');
       cells[i].id = ''; cells[i].innerHTML = left- (42-i-1);
-      cells[i].setAttribute('next');
+      cells[i].setAttribute('next',true);
       addClass(cells[i].parentElement,'off-month');
     }
   };
   drawcalender();
+  ondatechange();
+
   next.onclick = function(){
     nextDay();drawcalender(); ondatechange();
   };
@@ -153,6 +128,7 @@ window.onload = function(){
     previousDay();drawcalender(); ondatechange();
   };
   minicalendar.onmousedown = function(e){e.preventDefault();};
+
   for ( var i = 0;  i < cells.length;  i++){
     cells[i].onclick = function(){
       var currentYear = date.getFullYear();
